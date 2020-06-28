49fc184c6035b925907d6e91743c13ab1d511219
mikdadayan <dadayan.mikayel@gmail.com>
2020-06-28 15:53:47 +0400

    Updates

---
+++ precache-manifest.11f0fef58e56c360fd55e0b9b517734a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b26355a8c5d4ea34c15c341b5cec09c",
+    "url": "/mikdadayan/monsters-rol.git/index.html"
+  },
+  {
+    "revision": "7ed21adb918333a1fcc2",
+    "url": "/mikdadayan/monsters-rol.git/static/css/main.0a6ac3ff.chunk.css"
+  },
+  {
+    "revision": "18fa4d57670466122833",
+    "url": "/mikdadayan/monsters-rol.git/static/js/2.985b3e87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mikdadayan/monsters-rol.git/static/js/2.985b3e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed21adb918333a1fcc2",
+    "url": "/mikdadayan/monsters-rol.git/static/js/main.2ca230c8.chunk.js"
+  },
+  {
+    "revision": "0e94c2f3eeace43d953a",
+    "url": "/mikdadayan/monsters-rol.git/static/js/runtime-main.eb5a3053.js"
+  }
+]);
